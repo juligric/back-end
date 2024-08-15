0faebb8f11ff6f1ab06c79f5c286b7395314ba6f
juligric <49377944@est.ort.edu.ar>
2024-08-15 15:47:57 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+/*array → todos es un mismo lugar → conjunto d elementos*/
+/*cada uno esta en un indece por ejmplo 0, 1, etc */
+/*se puede declar de dos manera*/
+/* para saber cuanots elementos hay en un array se usa lenght--> me devuleve la cnatidad ed e elemntos
+
+
+let autos=  [“Honda” “Bmw” “Audi”]; /*array → todos es un mismo lugar → conjunto d elementos
+let frutas =[]
+
+alert(autos[1])
+frutas[0] ="bananas";
+frutas[1] = "frutillas";
+autos[2] = "bmw";
+frutas.push ("mandarina")  la funcion push le agraga un elmento a frutas*/
+
+var = x
